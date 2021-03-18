6ed3b1c6fa83508e2a33dd185c040954442458b4
luyang <luyang>
2021-03-18 23:12:43 +0800

    update

--- arch.sh
+++ arch.sh
@@ -21,22 +21,21 @@ check_and_setup "archlinuxcn" "$ARCHLINUXCN" "$ARCHLINUXCN_TO" 1
 # /etc/pacman.conf already backup above adding ARCHLINUXCN
 sudo sed -i 's/\#Color/Color' /etc/pacman.conf
 
-sudo pacman -Sy archlinux-keyring archlinuxcn-keyring
-sudo pacman -Sy yay
-sudo pacman -Sy firefox thunar xfce4-terminal
-sudo pacman -Sy nerd-fonts-fira adobe-source-han-sans-otc-fonts adobe-source-han-serif-otc-fonts wqy-microhei wqy-microhei-lite ttf-roboto
-sudo pacman -Sy base-devel devtools
-sudo pacman -Sy ctags cscope gcc minicom visual-studio-code-bin
-sudo pacman -Sy polipo tor proxychains veracrypt
-sudo pacman -Sy zsh tmux neovim python-pynvim neofetch python-pip
+sudo pacman -Sy archlinux-keyring archlinuxcn-keyring yay \
+		nerd-fonts-fira adobe-source-han-sans-otc-fonts adobe-source-han-serif-otc-fonts \
+		wqy-microhei wqy-microhei-lite ttf-roboto ttf-wps-fonts \
+		fcitx5 fcitx5-chinese-addons fcitx5-qt kcm-fcitx5 fcitx5-im fcitx5-pinyin-zhwiki fcitx5-material-color fcitx5-rime \
+		base-devel devtools ctags cscope gcc minicom python-pynvim python-pip \
+		zsh tmux neovim visual-studio-code-bin  \
+		polipo tor proxychains dnscrypt-proxy unbound \
+		firefox thunar xfce4-terminal lxappearance arc-gtk-theme arc-icon-theme \
+		weechat audacity mpv mplayer vlc ffmpeg openshot uget aria2 xsel xclip calibre \
+		veracrypt graphviz wget curl  neofetch 
 # sudo pacman -Sy fcitx fcitx-rime fcitx-gtk2 fcitx-gtk3 fcitx-qt4 fcitx-qt5 fcitx-configtool
-sudo pacman -Sy weechat audacity mpv mplayer vlc ffmpeg openshot uget aria2 xsel xclip
-sudo pacman -Sy ttf-wps-fonts calibre
-sudo pacman -Sy fcitx5 fcitx5-chinese-addons fcitx5-qt kcm-fcitx5 fcitx5-im fcitx5-pinyin-zhwiki fcitx5-material-color fcitx5-rime
-sudo pacman -Sy graphviz wget curl dnscrypt-proxy unbound
+
 # https://mirrors.tuna.tsinghua.edu.cn/help/AUR/
 yay --aururl "https://aur.tuna.tsinghua.edu.cn" --save
-yay -Sy wps-office-cn xmind-zen vscode-bin astah-professional
+yay -Sy wps-office-cn xmind-zen astah-professional
 
 # FCITX=$(echo -e"
 # \nexport XIM_PROGRAM=fcitx
